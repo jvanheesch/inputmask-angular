5b908f51fbc26a6ed0c4fe21ad289244d2f41e59
jvanheesch <joris.vanheesch@ae.be>
2020-09-25 20:54:14 +0200

    Created inputmask example, demonstrating that cut/paste does not change the model value.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import Inputmask from "inputmask";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'inputmask-angular';
+export class AppComponent implements AfterViewInit {
+    @ViewChild('someInput') someInput: ElementRef;
+    someModelValue: string;
+
+    ngAfterViewInit(): void {
+        Inputmask().mask(this.someInput.nativeElement)
+    }
 }
